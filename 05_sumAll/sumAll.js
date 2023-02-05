e5f1dd2aa8c3655ac5ed0d5f7f3ef8d2b8c5b51e
PabloPPG <pabloppg1992@gmail.com7.noreply.github.com>
2023-02-05 21:37:17 +0100

    Exercice resolution & test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  const greater = num1 < num2 ? num2 : num1;
+  let sum = num1 + num2;
+  for (let i = 1; i <= Math.abs(num1 - num2) - 1; i++) {
+    sum += greater - i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
